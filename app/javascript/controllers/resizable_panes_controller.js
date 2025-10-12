b1b8ea9d57d5147ab918274a2c06b7c7b0ade395
Carl Zulauf <carl@unabridgedsoftware.com>
2025-10-12 16:30:24 +0000

    Allow regex session panes to be resized

---
+++ app/javascript/controllers/resizable_panes_controller.js
@@ -0,0 +1,43 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.stopper = this.stopResizing.bind(this)
+    this.element.querySelectorAll(".vertical-gutter, .horizontal-gutter").forEach(el => {
+      el.addEventListener("mousedown", this.startResizing.bind(this))
+    });
+  }
+
+  startResizing(event) {
+    this.resizeTarget = event.target
+    if (this.resizeTarget.classList.contains("vertical-gutter")) {
+      this.resizer = this.horizontalResize.bind(this)
+    } else {
+      this.resizer = this.verticalResize.bind(this)
+    }
+
+    window.addEventListener("mouseup", this.stopper)
+    window.addEventListener("mousemove", this.resizer)
+  }
+
+  stopResizing(event) {
+    window.removeEventListener("mouseup", this.stopper)
+    window.removeEventListener("mousemove", this.resizer)
+  }
+
+  horizontalResize(event) {
+    const width = this.resizeTarget.parentElement.offsetWidth
+    const left = this.resizeTarget.previousElementSibling
+    const right = this.resizeTarget.nextElementSibling
+    left.style.width = `${event.clientX}px`
+    right.style.width = `${(width - event.clientX)}px`
+  }
+
+  verticalResize(event) {
+    const height = this.resizeTarget.parentElement.offsetHeight
+    const top = this.resizeTarget.previousElementSibling
+    const btm = this.resizeTarget.nextElementSibling
+    top.style.height = `calc(${event.clientY}px - 2rem)`
+    btm.style.height = `calc(${(height - event.clientY)}px - 2rem)`
+  }
+}
