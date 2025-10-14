0d118901d564ce0aeb3b4eb7861ce596ffd2302b
Carl Zulauf <carl@unabridgedsoftware.com>
2025-10-14 02:46:55 -0500

    Link title to root when were on regex domain, and don't treat localhost as regex

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
   def regex_domain?
-    ["localhost", "regex.mrks.io"].member?(request.host)
+    ["regex.mrks.io"].member?(request.host)
   end
   helper_method :regex_domain?
 end
